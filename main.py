a33b8df4fb0947d4e8dcaaa79242f509b26ae87c
vojtechtroszok <troszok.vojtech@seznam.cz>
2023-02-19 10:54:09 +0100

    initial commit

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ class MyCommandLine(Cmd):
         self.customer_registry = customer_registry
         self.prompt = "> "
         self.intro = """
-Tohle je fakt hnusnej program a kdo ho používá je proklet a o půlnoci přijde modrý dinosaurus a obrátí mu řiť naruby
+Vítejte v evidenci osob, pro nápovědu vypište help, pro konec exit
         """
 
     def do_vytvor(self, line):
@@ -28,16 +28,16 @@ Tohle je fakt hnusnej program a kdo ho používá je proklet a o půlnoci přijd
         builder.first_name = input("Zadej jméno: ")
         builder.last_name = input("Zadej Příjmení: ")
         builder.age = input("Zadej věk: ")
+        builder.phone_number = input("Zadej tel. č.: ")
         self.customer_registry.append(builder.build())
         print("Zákazník vytvořen\n")
 
     def do_vypis(self, line):
         """Vypíše všechny zákazníky v evidenci"""
         print("Seznam zákazníků:")
-        """verze 1"""
-        my_table = PrettyTable(["Jmeno", "Prijmeni", "Vek"])
+        my_table = PrettyTable(["Jmeno", "Prijmeni", "Vek", "Tel. č."])
         for customer in self.customer_registry:
-            my_table.add_row([customer.first_name, customer.last_name, customer.age])
+            my_table.add_row([customer.first_name, customer.last_name, customer.age, customer.phone_number])
             """print(customer.first_name, customer.last_name, customer.age)"""
 
         print(my_table)
@@ -46,34 +46,17 @@ Tohle je fakt hnusnej program a kdo ho používá je proklet a o půlnoci přijd
         print(" ")
 
 
-
     def do_vyhledej(self, line):
         """Vyhledá zadaného zákazníka"""
-        """sname = input("Zadej hledané jméno: ")
-        for customer in self.customer_registry:
-            if sname == customer.first_name or sname == customer.last_name:
-                print("Je tu")"""
-
         sname = input("Zadej hledané jméno: ")
-        my_table_vypis = PrettyTable(["Jmeno", "Prijmeni", "Vek"])
+        my_table_vypis = PrettyTable(["Jmeno", "Prijmeni", "Vek", "Tel. č."])
         for customer in filter(lambda customer: sname in [customer.first_name, customer.last_name], self.customer_registry):
-            my_table_vypis.add_row([customer.first_name, customer.last_name, customer.age])
+            my_table_vypis.add_row([customer.first_name, customer.last_name, customer.age, customer.phone_number])
 
         if len(my_table_vypis.rows) > 0:
             print(my_table_vypis)
         else:
-            print("Našel jsen veký hovno")
-        return
-
-        for customer in self.customer_registry:
-            if sname == customer.first_name or sname == customer.last_name:
-                my_table_vypis.add_row([customer.first_name, customer.last_name, customer.age])
-                """print(customer.first_name, customer.last_name, customer.age)"""
-        else:
             print("Záznam nenalezen")
-        print(my_table_vypis)
-
-
 
 
     def do_exit(self, line):
